c333101ec7211a2851521f56e750afac40c22ba9
Senichkaa <super-kapanaiko406@ukr.net>
2023-09-07 00:51:55 +0300

    fixed repo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -43,6 +43,20 @@ export class App extends Component {
     }));
   };
 
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
   render() {
     return (
       <div>
